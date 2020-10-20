cf57ed527931d61279af9c7e14e698d0cfac8aad
Jaisrith Tella <jaisrith@gmail.com>
2020-10-20 19:47:04 +0000

    Added mypyapptestproxyprobes/

---
+++ mypyapptestproxyprobes/server.py
@@ -0,0 +1,38 @@
+import tornado.ioloop
+import tornado.web
+import pymysql
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        title = "DANGER"
+        bgcolor = "darkred"
+        self.render("template.html",title=title,bgcolor=bgcolor)
+        print(self.request)
+
+class ProbeHandler(tornado.web.RequestHandler):
+    def get(self):
+        try:
+            db = pymysql.connect('localhost',"mypyappdbuser","NOTpassword","mypyappdb")
+            cursor = db.cursor()
+            cursor.execute("SELECT VERSION()")
+            result = cursor.fetchone()
+            title = f"OK DB Version {result}" 
+            bgcolor = "limegreen"
+            self.set_status(200)
+        except pymysql.Error as e:
+            title = f"NOT OK {e}"
+            bgcolor = "darkred"
+            self.set_status(503)
+        self.render("template.html",title=title,bgcolor=bgcolor)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/",MainHandler),
+        (r"/isalive",ProbeHandler),
+           ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
